0c45591b7aa0bcc5f094a193f0ca2b850f974ffb
Macaskill96 <levchuk.r.s.96@gmail.com>
2023-09-02 01:47:55 +0200

    aungular part1

---
+++ src/app/constants/urls.ts
@@ -0,0 +1,13 @@
+const baseUrl = 'https://jsonplaceholder.typicode.com'
+
+const users = `${baseUrl}/users`
+const posts = `${baseUrl}/posts`
+
+const urls = {
+  users:{
+    base:users,
+    byId:(id:number):string => `${users}/${id}`
+  }
+}
+
+export {urls}
